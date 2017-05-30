9513bc6abf5406f07fb9c8f628a996a59a7e9eb7
Stephen Colbert <stephen.j.colbert@gmail.com>
2017-05-30 14:54:59 -0600

    converted StartSmokingPage into a class so I can use React lifecycle methods

--- src/main/resources/public/js/containers/StartSmokingPage.js
+++ src/main/resources/public/js/containers/StartSmokingPage.js
@@ -3,26 +3,38 @@ import PlanSelector from '../components/PlanSelector';
 import { connect } from 'react-redux';
 import { setPlans } from '../actions/planActions.js';
 
-const StartSmokingPage = (props) => (
-    <div>
-        <title>StartSmokingPage Page</title>
-        <h1>Start Smoking Page</h1>
-        <PlanSelector plans={props.planState.plans} />
-    </div>
-)
+class StartSmokingPage extends React.Component {
+    constructor(props) {
+        super(props);
 
-const mapStateToProps = (state) => {
-    return {
-        planState: state.planState
+
+    }
+
+    render() {
+        return (
+            <div>
+                <title>StartSmokingPage Page</title>
+                <h1>Start Smoking Page</h1>
+                <PlanSelector plans={this.props.planState.plans}/>
+            </div>
+        )
     }
 }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        getPlans: () => {
-            dispatch( getPlans() );
+
+    const mapStateToProps = (state) => {
+        return {
+            planState: state.planState
         }
     }
-}
+
+    const mapDispatchToProps = (dispatch) => {
+        return {
+            getPlans: () => {
+                dispatch( getPlans() );
+            }
+        }
+    }
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(StartSmokingPage);
