41bbbe7d07c2928231f7bf408164dfbbec09d6b7
Stephen Colbert <stephen.j.colbert@gmail.com>
2017-05-30 16:05:23 -0600

    Got Async Wired Up for the PlanSelector

--- src/main/resources/public/js/actions/planActions.js
+++ src/main/resources/public/js/actions/planActions.js
@@ -1,8 +1,21 @@
+import $ from 'jquery';
+
 export function getPlans() {
-    return ({
-        type: "GET_PLANS",
-        payload: [{id: '3', name: 'Plan Retrieved From Action'}, {id: '4', name:'Another Plan Returned From Action'}]
-    });
+    return dispatch => {
+        $.ajax({
+            url: 'http://localhost:8080/api/v1/TempTiming',
+            success: (data) => {
+                dispatch ({
+                    type: "GET_PLANS",
+                    payload: data
+                })},
+            dataType: 'json'
+        });
+    }
+    // return ({
+    //     type: "GET_PLANS",
+    //     payload: [{id: '3', name: 'Plan Retrieved From Action'}, {id: '4', name:'Another Plan Returned From Action'}]
+    // });
 }
 
 export function addPlan(){
--- src/main/resources/public/js/store.js
+++ src/main/resources/public/js/store.js
@@ -1,7 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import planReducer from './reducers/PlanReducer.js';
 import sessionReducer from './reducers/sessionReducer.js';
 
-export default createStore(combineReducers({planState: planReducer, sessionState: sessionReducer}), {}, applyMiddleware(logger));
+export default createStore(
+    combineReducers({planState: planReducer, sessionState: sessionReducer}),
+    {},
+    applyMiddleware(logger, thunk));
