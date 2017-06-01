bdbc22e5795a54d246534b88154e18129f83fd59
Stephen Colbert <stephen.j.colbert@gmail.com>
2017-06-01 16:45:06 -0600

    Created function to convert farenheit to kelvin

--- src/main/resources/public/js/helpers/TemperatureConverter.js
+++ src/main/resources/public/js/helpers/TemperatureConverter.js
@@ -1,3 +1,7 @@
 module.exports.convertKelvinToFarenheit = (kelvinTemp) => {
-        return ((9/5)*(kelvinTemp - 273) + 32);
-    }
+    return ((9/5)*(kelvinTemp - 273) + 32);
+}
+
+module.exports.convertFarenheitToKelvin = (farenheitTemp) => {
+    return (farenheitTemp/1.8) + (459.4/1.8);
+}
--- src/main/resources/public/js/tests/testTemperatureConverter.js
+++ src/main/resources/public/js/tests/testTemperatureConverter.js
@@ -6,3 +6,10 @@ describe('convertKelvinToFarenheit', function(){
         assert(converter.convertKelvinToFarenheit(293) === 68);
     })
 })
+
+describe('convertFarenheitToKelvin', function(){
+    it('returns 310 when given 98.6', function(){
+        console.log(converter.convertFarenheitToKelvin(98.6));
+        assert(converter.convertFarenheitToKelvin(98.6) === 310)
+    })
+})
