5a574fa7e82d3e92b51be05964ebaa6deb2dee29
Stephen Colbert <stephen.j.colbert@gmail.com>
2017-06-01 15:55:28 -0600

    Added a Create Turn Off Criteria page and associated routing

--- src/main/resources/public/js/components/Main.js
+++ src/main/resources/public/js/components/Main.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 import StartSmokingPage from '../containers/StartSmokingPage';
 import CreateSmokingPlanPage from '../containers/CreateSmokingPlanPage';
 import PreferencesPage from '../containers/PreferencesPage';
+import CreateTurnOffCriteriaPage from '../containers/CreateTurnOffCriteriaPage';
 
 const Main = () => (
     <main>
         <Switch>
             <Route exact path="/" component={StartSmokingPage} />
+            <Route path="/createTurnOffCriteria" component={CreateTurnOffCriteriaPage} />
             <Route path="/createSmokingPlan" component={CreateSmokingPlanPage} />
             <Route path="/preferences" component={PreferencesPage} />
         </Switch>
---
+++ src/main/resources/public/js/containers/CreateTurnOffCriteriaPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CreateTurnOffCriteriaPage = (props) => {
+    return (
+        <div>
+            <title>Create Turn Off Criteria Page</title>
+            <h1>Create Turn Off Criteria Page</h1>
+        </div>
+    )
+}
+
+
+export default CreateTurnOffCriteriaPage;
